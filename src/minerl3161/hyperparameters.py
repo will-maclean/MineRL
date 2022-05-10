fae9d22489ca8ec2830a95e8d45b7c4df341d1ce
Jade Davis <jdav0023@student.monash.edu>
2022-05-11 00:00:42 +1000

    Adding init, stubs for trainer.py, adding hyperparameters.py

---
+++ src/minerl3161/hyperparameters.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class BaseHyperparameters:
+    lr: float = 2.5e-4
+    time_steps: int = 100000
+    batch_size: int = 32
+    buffer_size_gathered: int = 100000
+    buffer_size_dataset: int = 100000
+    gamma: float = 0.99
+    
+
+@dataclass
+class DQNHyperparameters:
+    eps_decay: float = 1e-5
+    eps_min: float = 0.01
+    eps_max: float = 1.0
+    model_hidden_layer_size: int = 64
--- src/minerl3161/trainer.py
+++ src/minerl3161/trainer.py
@@ -1,2 +1,39 @@
-class Trainer:
-    pass
+import numpy as np
+import torch
+from torch.optim import Optimizer, Adam
+import gym
+
+from agent import BaseAgent
+from hyperparameters import BaseHyperparameters
+from buffer import ReplayBuffer
+from evaluator import Evaluator
+
+class BaseTrainer:
+    def __init__(self, env: gym.Env, agent: BaseAgent, hyperparameters: BaseHyperparameters) -> None:
+        self.env = env
+        self.agent = agent
+        self.hp = hyperparameters
+        self.gathered_transitions = ReplayBuffer(self.hp.buffer_size_gathered)
+        self.dataset_transitions = ReplayBuffer(self.hp.buffer_size_dataset)
+        self.evaluator = Evaluator(env)
+    
+    def train() -> None:
+        pass
+
+    def _gather(steps: int) -> None:
+        pass
+
+    def _train_step(step: int) -> None:
+        pass
+
+    def _housekeeping(step: int) -> None:
+        # TODO: Better name for this method
+        pass
+
+
+class DQNTrainer(BaseTrainer):
+    def __init__(self, optim: Optimizer = None) -> None:
+        self.optim = optim if optim is not None else Adam(self.agent.q1.parameters(), lr=self.hp.lr)
+    
+    def _calc_loss(batch: dict) -> torch.Tensor:
+        pass
