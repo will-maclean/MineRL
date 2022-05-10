0978591e184738e187e95aadcf1f937ed4ea422a
Will Maclean <macleanwill1@gmail.com>
2022-05-11 00:06:18 +1000

    working on agent and model

--- src/minerl3161/agent.py
+++ src/minerl3161/agent.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
+from copy import deepcopy
+import random
+from typing import Tuple, Union
+
 import numpy as np
+import torch as th
 
 from minerl3161.models import DQNNet
 
@@ -20,12 +25,46 @@ class BaseAgent(ABC):
 
 
 class DQNAgent(BaseAgent):
-    def __init__(self) -> None:
+    def __init__(self, state_shape: Tuple[int], n_actions: int, device: str, hyperparams: Hyperparameters) -> None:  # FIXME @ Jade
         super().__init__()
-        self.model = DQNNet()
+        self.device = device
+        self.hyperparams = hyperparams
+
+        self.q1 = DQNNet(state_shape, n_actions, hyperparams.model_hidden_layer_size).to(device)  # FIXME @ Jade
+        
+        self.q2 = deepcopy(self.q1)
+        self.q2.requires_grad_(False)
+        self.q2.eval()
     
-    def act(self, state):
-        raise NotImplementedError()
+    def act(self, state: np.ndarray) -> np.ndarray:
+        state = th.from_numpy(state).to(self.device).unsqueeze(0)
+
+        q_vals = self.q1(state)
+
+        action = q_vals.squeeze().argmax().detach().cpu().numpy()
+
+        return action
+    
+    def eps_greedy_act(self, state: Union[np.ndarray, th.Tensor], step: int) -> Union[np.ndarray, th.Tensor]:
+        if type(state) == np.np.ndarray:
+            state = th.from_numpy(state).to(self.device).unsqueeze(0)
+            was_np = True
+        else:
+            was_np = False
+        
+
+        eps = epsilon_decay()  # TODO
+
+        if random.random() < eps:
+            pass
+        else:
+            pass
+
+        if was_np:
+            action = action.detach().cpu().numpy()
+        
+        return action
+    
     
     def save(self, path):
         raise NotImplementedError()
--- src/minerl3161/models.py
+++ src/minerl3161/models.py
@@ -1,10 +1,36 @@
+from typing import Tuple
 import torch as th
 from torch import nn
 
 
 class DQNNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, state_shape: Tuple[int], n_actions: int, layer_size=64) -> None:
         super().__init__()
+
+        self.feature_extractor = lambda x: x  # TODO: create feature extractor based on state
+
+        sample_input = th.ones((1, *state_shape))
+        n_hidden_features = self.feature_extractor(sample_input).flatten(1).shape[1]
+
+        # duelling architecture
+        self.value = nn.Sequential(
+            nn.Linear(layer_size, layer_size, bias=True),
+            nn.ReLU(),
+            nn.Linear(layer_size, 1)
+        )
+
+        self.advantage = nn.Sequential(
+            nn.Linear(layer_size, layer_size, bias=True),
+            nn.ReLU(),
+            nn.Linear(layer_size, n_actions)
+        )
+
     
     def forward(self, x: th.Tensor) -> th.Tensor:
-        pass
+        x = self.feature_extractor(x)
+
+        v = self.value(x)
+
+        advantage = self.advantage(x)
+
+        return v + (advantage - advantage.mean())
