a6ba375221781dfaecb9d42d7750f009c4cd0e85
Will Maclean <macleanwill1@gmail.com>
2022-08-11 22:57:08 +1000

    implemented conftest with fixture for faster tests

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+import gym
+import minerl
+
+
+@pytest.fixture(scope="session")
+def minerl_env():
+    env_name = "MineRLObtainDiamondShovel-v0"
+    env = gym.make(env_name)
+
+    env.reset()
+
+    yield env
+
+    env.close()
--- tests/test_env_wrapper.py
+++ tests/test_env_wrapper.py
@@ -4,16 +4,15 @@ from minerl3161.hyperparameters import DQNHyperparameters
 
 from minerl3161.wrappers import mineRLObservationSpaceWrapper
 
-def test_env_wrapper_basic():
-    env_name = "MineRLObtainDiamondShovel-v0"
-    unwrapped_env = gym.make(env_name)
+
+def test_env_wrapper_basic(minerl_env):
 
     w = 16
     h = 16
     features = ['pov', 'stone_sword', 'stonecutter', 'stone_shovel']
     stack = 4
 
-    env = mineRLObservationSpaceWrapper(unwrapped_env, frame=stack, features=features, downsize_width=w, downsize_height=h)
+    env = mineRLObservationSpaceWrapper(minerl_env, frame=stack, features=features, downsize_width=w, downsize_height=h)
 
     assert env.observation_space['pov'].shape == (stack, w, h)
 
@@ -22,15 +21,13 @@ def test_env_wrapper_basic():
     assert state['pov'].shape == (stack, w, h)
     assert state['inventory'].shape == (3,)
 
-def test_env_wrapper_full():
-    env_name = "MineRLObtainDiamondShovel-v0"
-    unwrapped_env = gym.make(env_name)
+def test_env_wrapper_full(minerl_env):
 
     w = 16
     h = 16
     features = DQNHyperparameters().inventory_feature_names
     stack = 4
-    env = mineRLObservationSpaceWrapper(unwrapped_env, frame=stack, features=features, downsize_width=w, downsize_height=h)
+    env = mineRLObservationSpaceWrapper(minerl_env, frame=stack, features=features, downsize_width=w, downsize_height=h)
 
     assert env.observation_space['pov'].shape == (stack, w, h)
 
