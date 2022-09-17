df777c82a19eadfdbe0cd898bfaab2994b08487d
Will Maclean <macleanwill1@gmail.com>
2022-09-17 15:03:14 +1000

    updating for large run

--- src/minerl3161/hyperparameters.py
+++ src/minerl3161/hyperparameters.py
@@ -9,10 +9,10 @@ class BaseHyperparameters:
     train_every: int = 1  # how many steps per train call
     evaluate_every: int = 10_000  # how many steps per evaluation call
     evaluate_episodes: int = 5  # how many episodes we complete each evaluation call
-    batch_size: int = 8  # batch size for training
+    batch_size: int = 64  # batch size for training
     buffer_size_gathered: int = 100000  # buffer size for gathered data
     buffer_size_dataset: int = (
-        1_000  # buffer size for the provided data i.e. how much provided data to use
+        20_000  # buffer size for the provided data i.e. how much provided data to use
     )
     gather_every: int = 1  # how often we collect transition data
     gather_n: int = 1  # how many transitions we collect at once
--- src/scripts/convert_dataset.py
+++ src/scripts/convert_dataset.py
@@ -10,7 +10,7 @@ from minerl3161.wrappers import MineRLWrapper
 from minerl3161.buffer import ReplayBuffer
 from minerl3161.hyperparameters import DQNHyperparameters
 
-def convert_dataset(env_name, out_path, hyperparams):
+def convert_dataset(env_name, out_path, hyperparams, save_every=5):
 
     env = gym.make(env_name)
 
@@ -22,16 +22,22 @@ def convert_dataset(env_name, out_path, hyperparams):
 
     action_set = MineRLWrapper.create_action_set(functional_acts=True, extracted_acts=True)
     
+    counter = 0
+
     for traj_name in tqdm(trajectory_names):
         state_buffer = np.zeros_like(empty_buffer)
         next_state_buffer = np.zeros_like(empty_buffer)
         for s, a, r, s_, d in data.load_data(traj_name):
 
+
             s, _, state_buffer = MineRLWrapper.convert_state(s, state_buffer=state_buffer, **dataclasses.asdict(hyperparams))
             a = MineRLWrapper.map_action(a, action_set=action_set)
             s_, _, next_state_buffer = MineRLWrapper.convert_state(s_, state_buffer=next_state_buffer, **dataclasses.asdict(hyperparams))
 
-            buffer.add(s, a, s_, r, d)
+            counter += 1
+            
+            if counter % save_every == 0:
+                buffer.add(s, a, s_, r, d)
 
             if buffer.full:
                 break
@@ -46,9 +52,10 @@ if __name__ == "__main__":
 
     parser.add_argument('--env_name', type=str, default='MineRLObtainDiamond-v0')
     parser.add_argument('--out_path', type=str, default='data/human-xp.pkl')
+    parser.add_argument('--save_every', type=int, default=5)
 
     args = parser.parse_args()
 
     hyperparams = DQNHyperparameters()
 
-    convert_dataset(env_name=args.env_name, out_path=args.out_path, hyperparams=hyperparams)
+    convert_dataset(env_name=args.env_name, out_path=args.out_path, hyperparams=hyperparams, save_every=args.save_every)
