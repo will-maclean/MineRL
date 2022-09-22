792c72259d1a31af9161b2305ed27430b1096d20
Jade Davis <jdav0023@student.monash.edu>
2022-09-22 13:37:33 +1000

    Creating PER class

--- src/minerl3161/buffer.py
+++ src/minerl3161/buffer.py
@@ -3,8 +3,10 @@ import pickle
 from collections import namedtuple
 from typing import Dict, List, Tuple
 from pathlib import Path
-
 import numpy as np
+import random
+
+from minerl3161.segment_tree import MinSegmentTree, SumSegmentTree
 
 Transition = namedtuple("Transition", ["s", "a", "s_", "r", "d"])
 
@@ -182,3 +184,86 @@ class ReplayBuffer:
             self.actions[idxs],
             {key: self.states[key][idxs] for key in self.feature_names},
             {key: self.next_states[key][idxs] for key in self.feature_names})
+
+
+class PrioritisedReplayBuffer(ReplayBuffer):
+
+    def __init__(
+        self, 
+        n: int, 
+        obs_space: Dict[str, np.ndarray], 
+        alpha: float = 0.6
+    ) -> None:
+        super(PrioritisedReplayBuffer, self).__init__(n, obs_space)
+
+        self.max_priority, self.tree_ptr = 1.0, 0
+        self.alpha = alpha
+        
+        # capacity must be positive and a power of 2.
+        tree_capacity = 1
+        while tree_capacity < self.max_size:
+            tree_capacity *= 2
+
+        self.sum_tree = SumSegmentTree(tree_capacity)
+        self.min_tree = MinSegmentTree(tree_capacity)
+
+    def add(
+        self,
+        state: np.ndarray,
+        action: np.ndarray,
+        next_state: np.ndarray,
+        reward: np.ndarray,
+        done: np.ndarray,
+    ) -> None:
+        super().add(state, action, next_state, reward, done)
+    
+        self.sum_tree[self.tree_ptr] = self.max_priority ** self.alpha
+        self.min_tree[self.tree_ptr] = self.max_priority ** self.alpha
+        self.tree_ptr = (self.tree_ptr + 1) % self.max_size
+
+    def sample(
+        self, 
+        batch_size: int
+    ) -> Dict[str, np.ndarray]:
+        pass
+
+    def update_priorities(self, indices: List[int], priorities: np.ndarray):
+        """Update priorities of sampled transitions."""
+        assert len(indices) == len(priorities)
+
+        for idx, priority in zip(indices, priorities):
+            assert priority > 0
+            assert 0 <= idx < len(self)
+
+            self.sum_tree[idx] = priority ** self.alpha
+            self.min_tree[idx] = priority ** self.alpha
+
+            self.max_priority = max(self.max_priority, priority)
+            
+    def _sample_proportional(self) -> List[int]:
+        """Sample indices based on proportions."""
+        indices = []
+        p_total = self.sum_tree.sum(0, len(self) - 1)
+        segment = p_total / self.batch_size
+        
+        for i in range(self.batch_size):
+            a = segment * i
+            b = segment * (i + 1)
+            upperbound = random.uniform(a, b)
+            idx = self.sum_tree.retrieve(upperbound)
+            indices.append(idx)
+            
+        return indices
+    
+    def _calculate_weight(self, idx: int, beta: float):
+        """Calculate the weight of the experience at idx."""
+        # get max weight
+        p_min = self.min_tree.min() / self.sum_tree.sum()
+        max_weight = (p_min * len(self)) ** (-beta)
+        
+        # calculate weights
+        p_sample = self.sum_tree[idx] / self.sum_tree.sum()
+        weight = (p_sample * len(self)) ** (-beta)
+        weight = weight / max_weight
+        
+        return weight
