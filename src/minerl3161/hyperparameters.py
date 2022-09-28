f83eeed98ab2dbc889050b684c835ebeba7886a0
will-maclean <41996719+will-maclean@users.noreply.github.com>
2022-09-28 15:56:52 +1000

    Sample refactor (#27)
    * refactoring
    
    * updating defaults
    
    * action bug
    
    * updating param bug

--- src/minerl3161/hyperparameters.py
+++ src/minerl3161/hyperparameters.py
@@ -1,18 +1,20 @@
 from dataclasses import dataclass, field
 from typing import List, Union
 
+from minerl3161.utils import linear_sampling_strategy
+
 
 @dataclass
 class BaseHyperparameters:
     train_steps: int = 2_000_000  # number of train_loop steps
-    burn_in: int = 5_000  # how many steps to loop for before starting training
+    burn_in: int = 10_000  # how many steps to loop for before starting training
     train_every: int = 1  # how many steps per train call
     evaluate_every: int = 50_000  # how many steps per evaluation call
     evaluate_episodes: int = 5  # how many episodes we complete each evaluation call
     batch_size: int = 256  # batch size for training
     buffer_size_gathered: int = 75_000  # buffer size for gathered data
     buffer_size_dataset: int = (
-        5_000  # buffer size for the provided data i.e. how much provided data to use
+        50_000  # buffer size for the provided data i.e. how much provided data to use
     )
     gather_every: int = 1  # how often we collect transition data
     gather_n: int = 1  # how many transitions we collect at once
@@ -29,21 +31,21 @@ class BaseHyperparameters:
 
     # sampling options
     sample_max: float = 1.0
-    sample_min: float = 0.1
-    sample_final_step: int = 1_500_000
+    sample_min: float = 0.05
+    sample_final_step: int = 15_000
 
 
 @dataclass
 class DQNHyperparameters(BaseHyperparameters):
     gamma: float = 0.99  # discount factor for Bellman Equation
     lr: float = 2.5e-4  # learning rate for model weights
-    eps_decay: float = 1_500_000  # decay factor for epsilon greedy strategy
+    eps_decay: float = 50_000  # decay factor for epsilon greedy strategy
     eps_min: float = 0.01  # min value for epsilon greedy strategy
     eps_max: float = 1.0  # max value for epsilon greedy strategy
     model_hidden_layer_size: int = 64  # layer size for hidden layers in neural net
     hard_update_freq: Union[
         int, None
-    ] = 10_000  # how ofter to do a hard copy from q1 to q2
+    ] = 30_000  # how ofter to do a hard copy from q1 to q2
     soft_update_freq: Union[
         int, None
     ] = 0  # how often to do a soft update from q1 to q2
@@ -52,8 +54,8 @@ class DQNHyperparameters(BaseHyperparameters):
     
     # these are the feature names that are passed into the model to learn on
     feature_names: List = field(default_factory=lambda:[
-        "pov",
-        "inventory",
+        # "pov",
+        # "inventory",
         "compass",
     ])
 
@@ -71,3 +73,4 @@ class RainbowDQNHyperparameters(DQNHyperparameters):
     alpha: float = 0.2
     beta_max: float = 1.0
     beta_min: float = 0.6
+    sampling_strategy: callable = linear_sampling_strategy
