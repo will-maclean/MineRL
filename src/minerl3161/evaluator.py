a187b205bfeb158348b58175b13559ccd209b945
Amir Toosi <54131619+amir-kt@users.noreply.github.com>
2022-10-03 22:26:37 +1100

    implemented evaluate function (#31)

--- src/minerl3161/evaluator.py
+++ src/minerl3161/evaluator.py
@@ -5,9 +5,53 @@ from minerl3161.agent import BaseAgent
 class Evaluator:
     def __init__(self, env) -> None:
         self.env = env
+        
+        self.env_interaction = {
+            "needs_reset": True,
+            "last_state": None,
+            "episode_return": 0,
+            "episode_length": 0
+        }
 
     def evaluate(self, agent: BaseAgent, episodes: int) -> dict:
-        return {}
+        self.env_interaction["needs_reset"] = True
+        
+        info = {
+            'episode_return': [],
+            'episode_length': []
+            }
+            
+        for _ in range(episodes):
+            done = False
+            
+            while not done:
+                if self.env_interaction['needs_reset']:
+                    state = self.env.reset()
+                    self.env_interaction['needs_reset'] = False
+                else:
+                    state = self.env_interaction["last_state"]
+                
+                action, _ = agent.act(state=state, train=True, step=self.t)
+                action = action.detach().cpu().numpy().item()
+
+                next_state, reward, done, info = self.env.step(action)
+
+                # self.env.render()
+
+                self.env_interaction["episode_return"] += reward
+                self.env_interaction["last_state"] = next_state
+                self.env_interaction["episode_length"] += 1
+                
+
+            info["episode_return"].append(self.env_interaction["episode_return"])
+            info['episode_length'].append(self.env_interaction["episode_length"])
+            
+            self.env_interaction["episode_length"] = 0
+            self.env_interaction["episode_return"] = 0
+            self.env_interaction["needs_reset"] = True
+            self.env_interaction["last_state"] = None
+
+        return info
 
     def create_media(self, agent: BaseAgent) -> dict:
         return {}
